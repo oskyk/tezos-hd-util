33ba7160b100ccdb16c96aa9b36b298363b2ce9a
Oskar Hladky <oskar.hladky@bitpanda.com>
2019-04-30 12:30:07 +0200

    secp256k1 signing

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(name='tezos-hd-util',
-      version='0.0.1',
+      version='0.0.2',
       packages=find_packages(),
-      install_requires=['chainside-btcpy-multi==0.2.77', 'pyblake2==1.1.2'],
+      install_requires=['chainside-btcpy-multi==0.2.77', 'pyblake2==1.1.2', 'secp256k1new==0.13.2'],
       description='Python tool for for tezos hd generation',
       author='Oskar Hladky',
       author_email='oskyks1@gmail.com',
--- tezoshd/tezoshd.py
+++ tezoshd/tezoshd.py
@@ -1,7 +1,38 @@
-from base58 import b58encode_check
+from base58 import b58encode_check, b58decode_check
 from pyblake2 import blake2b
 import btcpy
+from binascii import hexlify, unhexlify
 from btcpy.structs.hd import ExtendedPrivateKey, ExtendedPublicKey
+import secp256k1
+
+
+TZ_VERSION = {
+    'tz1': '0000',
+    'tz2': '0001',
+    'tz3': '0002'
+}
+
+
+def blake2b_32(v=b''):
+    return blake2b(v, digest_size=32)
+
+
+def numToZarith(num):
+    bit_str = bin(num)[2:]
+    while len(bit_str) % 7 is not 0:
+        bit_str = '0' + bit_str
+    result = ''
+    for i in range(len(bit_str), 0, -7):
+        bit_str_section = bit_str[i-7:i]
+        if i is 7:
+            bit_str_section = '0' + bit_str_section
+        else:
+            bit_str_section = '1' + bit_str_section
+        hex_str = '{:x}'.format(int(bit_str_section, 2))
+        if len(hex_str) % 2 == 1:
+            hex_str = '0' + hex_str
+        result += hex_str
+    return result
 
 
 class XPrv(object):
@@ -42,3 +73,48 @@ class XPub(object):
         tz2 = b'\x06\xa1\xa1'
         pkh = blake2b(data=self.key.key.serialize(), digest_size=20).digest()
         return b58encode_check(tz2 + pkh)
+
+
+class Transaction(object):
+    def __init__(self, branch, source, fee, counter, gas_limit, storage_limit, amount, destination):
+        self.branch = branch
+        self.source = source
+        self.fee = fee
+        self.counter = counter
+        self.gas_limit = gas_limit
+        self.storage_limit = storage_limit
+        self.amount = amount
+        self.destination = destination
+
+    def _cleaned_address(self, addr):
+        if addr[:2] == 'tz':
+            return TZ_VERSION[addr[:3]] + hexlify(b58decode_check(addr)).decode()[6:]
+        elif addr[:2] == 'KT':
+            return '01' + hexlify(b58decode_check(addr)).decode()[6:] + '00'
+        else:
+            raise KeyError('Unknown address: {}'.format(addr))
+
+    def serialize(self):
+        result = hexlify(b58decode_check(self.branch)).decode()[4:]
+        result += '08'  # tag for tx
+        result += '0000'  # source version
+        result += self._cleaned_address(self.source)
+        result += numToZarith(self.fee)
+        result += numToZarith(self.counter)
+        result += numToZarith(self.gas_limit)
+        result += numToZarith(self.storage_limit)
+        result += numToZarith(self.amount)
+        result += self._cleaned_address(self.destination)
+        result += '00'  # params
+        return result
+
+    def signature(self, key):
+        raw_tx = self.serialize()
+        sk_str = b58decode_check(key)[4:]
+        pk = secp256k1.PrivateKey(sk_str)
+        signature = pk.ecdsa_serialize_compact(
+            pk.ecdsa_sign(b'\x03' + unhexlify(raw_tx), digest=blake2b_32))
+        return hexlify(signature)
+
+    def signed(self, key):
+        return self.serialize() + self.signature(key)
