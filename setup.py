7a07854f4b4254013568b1b8acb6d832aced4ccb
Oskar Hladky <oskar.hladky@bitpanda.com>
2019-10-23 17:32:08 +0200

    version bump

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(name='tezos-hd-util',
-      version='0.1.5',
+      version='0.1.6',
       packages=find_packages(),
       install_requires=['chainside-btcpy-multi==0.2.78', 'pyblake2==1.1.2', 'secp256k1new==0.13.2'],
       description='Python tool for for tezos hd generation',
       author='Oskar Hladky',
       author_email='oskyks1@gmail.com',
       url='https://github.com/oskyk/tezos-hd-util',
-      download_url='https://github.com/oskyk/tezos-hd-util/archive/0.1.5.tar.gz',
+      download_url='https://github.com/oskyk/tezos-hd-util/archive/0.1.6.tar.gz',
       python_requires='>=3',
       keywords=['tezos', 'hd', 'address'],
       classifiers=[
