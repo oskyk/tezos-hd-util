6813512997d04b9c2b907fceefa2e90e053cc40c
Taneli Hukkinen <hukkinj1@users.noreply.github.com>
2020-01-29 14:16:43 +0100

    Dont pin exact versions

--- setup.py
+++ setup.py
@@ -4,7 +4,11 @@ from setuptools import find_packages
 setup(name='tezos-hd-util',
       version='0.1.6',
       packages=find_packages(),
-      install_requires=['chainside-btcpy-multi==0.2.78', 'pyblake2==1.1.2', 'secp256k1new==0.13.2'],
+      install_requires=[
+            'chainside-btcpy-multi>=0.2.78,<0.3.0',
+            'pyblake2>=1.1.2,<2.0.0',
+            'secp256k1new>=0.13.2,<0.14.0',
+      ],
       description='Python tool for for tezos hd generation',
       author='Oskar Hladky',
       author_email='oskyks1@gmail.com',
